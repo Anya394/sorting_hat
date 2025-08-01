44bb86a22cbcc615858028c3c7152a954cc216d9
Мокрецова Анна <anmo_pm02@mail.ru>
2025-08-01 22:44:42 +0400

    project init

---
+++ eslint.config.mjs
@@ -0,0 +1,56 @@
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import { FlatCompat } from "@eslint/eslintrc";
+import eslintPluginPrettier from "eslint-plugin-prettier";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: {
+    rules: {
+      "no-cond-assign": "error",
+      "no-constant-condition": "error",
+    }
+  }
+});
+
+const eslintConfig = [
+  ...compat.extends(
+    "next/core-web-vitals",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended"
+  ),
+  {
+    plugins: {
+      prettier: eslintPluginPrettier,
+    },
+    rules: {
+      // Основные правила ESLint
+      "no-console": "warn",
+      "no-unused-vars": "off", // Отключение в пользу typescript-eslint
+      "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+      "react-hooks/exhaustive-deps": "warn",
+      
+      // Правила форматирования
+      "prettier/prettier": [
+        "error",
+        {
+          printWidth: 100,
+          tabWidth: 2,
+          useTabs: false,
+          semi: true,
+          singleQuote: false,
+          trailingComma: "es5",
+          bracketSpacing: true,
+          jsxBracketSameLine: false,
+          arrowParens: "always",
+          endOfLine: "auto",
+        },
+      ],
+    },
+  },
+];
+
+export default eslintConfig;
